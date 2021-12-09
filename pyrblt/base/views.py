8925e008f3439fe4819d252db3eea6d8aa0b745c
robertolt <rl.beto.lorenzoni@gmail.com>
2021-12-09 14:46:52 -0300

    created base app

--- pyrblt/base/views.py
+++ pyrblt/base/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def home(request):   #this always need to have 'request' as parameter
-    return HttpResponse('Olá Django')
+def home(request):
+    return HttpResponse('Hello Django')
