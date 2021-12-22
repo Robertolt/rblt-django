1e1ac1cdcb0123f980a74028af2e5132ed4b1039
robertolt <rl.beto.lorenzoni@gmail.com>
2021-12-21 21:41:52 -0300

    add secret key

--- pyrblt/settings.py
+++ pyrblt/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e6cpd1t6j@^9guvi32-5x0volbvs6589@uz9bw(^2ri=x(wab='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
