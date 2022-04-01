7a9f37ea555fd1c2ad68a127d1b67488f7e18f20
robertolt <rl.beto.lorenzoni@gmail.com>
2022-04-01 16:37:34 -0300

    exploring slug feature

--- pyrblt/aperitivos/views.py
+++ pyrblt/aperitivos/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    videos = {
+        'motivacao': {'titulo': 'Vídeo Aperitivo: Motivação', 'vimeo_id': 694576600},
+        'instalacao-windows': {'titulo': 'Instalando Windows', 'vimeo_id': 695012786},
+    }
+    video = videos[slug]
+    return render(request, 'aperitivos/video.html', context={'video': video})
