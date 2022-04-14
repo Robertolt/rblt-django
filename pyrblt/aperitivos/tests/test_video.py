eb930eaa0d9f35dd5e1ea2d0bc2f8575d67b36ac
robertolt <rl.beto.lorenzoni@gmail.com>
2022-04-13 21:25:30 -0300

    testing_with_database

--- pyrblt/aperitivos/tests/test_video.py
+++ pyrblt/aperitivos/tests/test_video.py
@@ -3,13 +3,12 @@ from django.urls import reverse
 
 from pyrblt.aperitivos.models import Video
 from pyrblt.django_assertions import assert_contains
+from model_mommy import mommy
 
 
 @pytest.fixture
 def video(db):
-    v = Video(slug='motivacao', titulo='Vídeo Aperitivo: Motivação', vimeo_id='694576600')
-    v.save()
-    return v
+    return mommy.make(Video)
 
 
 @pytest.fixture
@@ -35,10 +34,4 @@ def test_titulo_video(resp, video):
 
 
 def test_conteudo_video(resp, video):
-    assert_contains(resp, '<div style="padding:75% 0 0 0;position:relative;"><iframe '
-                          f'src="https://player.vimeo.com/video/{video.vimeo_id}?h=4451873560&amp;badge=0&amp"'
-                          ';autopause=0&amp '
-                          ';player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; '
-                          'picture-in-picture" allowfullscreen '
-                          'style="position:absolute;top:0;left:0;width:100%;height:100%;" title="Seasonal Sale - '
-                          'Spring"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>')
+    assert_contains(resp, f'<iframe src="https://player.vimeo.com/video/{video.vimeo_id}')
