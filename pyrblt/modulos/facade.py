de34dd245cc9d402a4fa712f47e442d937c713d2
robertolt <rl.beto.lorenzoni@gmail.com>
2022-05-04 12:32:37 -0300

    data_migration

--- pyrblt/modulos/facade.py
+++ pyrblt/modulos/facade.py
@@ -8,4 +8,4 @@ def listar_modulos_ordenados() -> List[Modulo]:
     Lista Módulos ordenados por títulos
     :return:
     """
-    return list(Modulo.objects.order_by('titulo').all())
+    return list(Modulo.objects.order_by('order').all())
